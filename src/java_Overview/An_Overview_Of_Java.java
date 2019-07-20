1627b53ad046a571a1b25b610c9f813c9add9925
rkverma05 <rohit.asp900@gmail.com>
2019-07-20 17:57:54 +0100

    First Commit

---
+++ src/java_Overview/An_Overview_Of_Java.java
@@ -0,0 +1,29 @@
+package java_Overview;
+
+public class An_Overview_Of_Java {
+
+	public static void main(String[] args) {
+		
+		
+		//System.out.println("My First Message");
+		
+		//@SuppressWarnings("unused")
+		
+		int n;
+		
+		n=100;
+		
+       System.out.println(n);
+
+        n=n*2;
+
+        System.out.println("N Value n=n*2");
+		System.out.println(n);
+		
+		
+		//OOP CONCEPT
+		//ENCAPSULATION-POLYMORPHISM-INHERITANCE
+		
+	}
+
+}
---
+++ src/java_Overview/Control_Statement.java
@@ -0,0 +1,33 @@
+package java_Overview;
+
+//Package name should always be in lower case like camel
+//Class name should always be defined in the CAMEL Case like Control_Statement
+//Operators are ==(Equals To) <(Less than) >(Greater than)
+
+
+public class Control_Statement {
+
+	public static void main(String[] args) {
+		
+		
+		//If Statement
+		
+		int a,b;
+		
+		a=10;
+		b=20;
+		
+		if (a>b)
+		
+System.out.println("A is greater than B"+a);
+		
+		else 
+			if (a==b)
+			System.out.println("A is equal to B"+a);
+			else 
+				if (a<b)
+					System.out.println("A is less than B"+a);
+		
+	}
+
+}
